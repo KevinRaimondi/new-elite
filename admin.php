347542c036e89a888d280621fbcb9acdd052d577
Kevin Raimondi <Kevin_Raimondi@outlook.com>
2017-11-30 11:10:54 -0200

    site

--- admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!empty($_SESSION['login_usu'])){
-	echo "Olá ".$_SESSION['nome_usu'].", Bem vindo <br>";
-	echo "<a href='sair.php'>Sair</a>";
-}else{
-	$_SESSION['msg'] = "Área restrita";
-	header("Location: login.php");	
-}
