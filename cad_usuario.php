6c0d7c7c2ebb2486f9c6bda4daa06965c292d39c
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-02 01:54:21 -0300

    testando

--- cad_usuario.php
+++ cad_usuario.php
@@ -1,23 +1,29 @@
 ﻿<?php 
 
 	require("conexao.php");
+
+
 	
 	session_start();
-		$_SESSION['email']			 = $_POST['email'];
-		$_SESSION['usuario']		 = $_POST['usuario'];
-		$_SESSION['password']		 = $_POST['password'];
-		$_SESSION['passwordConfirm'] = $_POST['passwordConfirm'];
-		$_SESSION['nome'] 			 = $_POST['nome'];
-		$_SESSION['sobrenome'] 		 = $_POST['sobrenome'];
-		$_SESSION['rg'] 			 = $_POST['rg'];
-		$_SESSION['cpf'] 			 = $_POST['cpf'];
-		$_SESSION['nascimento'] 	 = $_POST['nascimento'];
-		$_SESSION['rua'] 			 = $_POST['rua'];
-		$_SESSION['numero'] 		 = $_POST['numero'];
-		$_SESSION['cep'] 			 = $_POST['cep'];
-		$_SESSION['bairro'] 		 = $_POST['bairro'];
-		$_SESSION['cidade'] 		 = $_POST['cidade'];
-		$_SESSION['estado'] 		 = $_POST['estado'];
+	
+	$email 			  = 	$_SESSION['email']			 = $_POST['email'];                 
+	$usuario		  = 	$_SESSION['usuario']		 = $_POST['usuario'];               
+	$password		  = 	$_SESSION['password']		 = $_POST['password'];              
+	$passwordConfirm  = 	$_SESSION['passwordConfirm'] = $_POST['passwordConfirm'];       
+	$nome 			  = 	$_SESSION['nome'] 			 = $_POST['nome'];                  
+	$sobrenome		  = 	$_SESSION['sobrenome'] 		 = $_POST['sobrenome'];             
+	$rg				  = 	$_SESSION['rg'] 			 = $_POST['rg'];                    
+	$cpf			  = 	$_SESSION['cpf'] 			 = $_POST['cpf'];                   
+					  =    	                                                                
+	$nascimento  	  = 	$_SESSION['nascimento'] 	 = $_POST['nascimento'];            
+					  =    	                                                                
+	$rua  			  = 	$_SESSION['rua'] 			 = $_POST['rua'];                   
+	$numero  		  = 	$_SESSION['numero'] 		 = $_POST['numero'];                
+	$cep  			  = 	$_SESSION['cep'] 			 = $_POST['cep'];                   
+	$bairro  		  = 	$_SESSION['bairro'] 		 = $_POST['bairro'];                
+	$cidade  		  = 	$_SESSION['cidade'] 		 = $_POST['cidade'];                
+	$estado  		  = 	$_SESSION['estado'] 		 = $_POST['estado'];                
+
 	
 		
 	if($password != $passwordConfirm){
@@ -28,6 +34,6 @@
 		header('Location: /cadastro/index.php?msg=2"');
 	}
 			
-	$query = mysqli_query ($conn,"INSERT INTO `usuario` (`id`, `login`, `senha`, `email`, `nome`, `sobrenome`, `rg`, `cpf`, `nascimento`, `rua`, `numero`, `cep`, `bairro`, `cidade`, `estado`) VALUES (NULL, '$_SESSION['usuario']', '$_SESSION['password']', '$_SESSION['email']', '$_SESSION['nome']', '$_SESSION['sobrenome']', '$_SESSION['rg']', '$_SESSION['cpf']', NULL, '$_SESSION['rua']', '$_SESSION['numero']', '$_SESSION['cep']', '$_SESSION['bairro']', '$_SESSION['cidade']', '$_SESSION['estado']')");
+	$query = mysqli_query ($conn,"INSERT INTO `usuario` (`id`, `login`, `senha`, `email`, `nome`, `sobrenome`, `rg`, `cpf`, `nascimento`, `rua`, `numero`, `cep`, `bairro`, `cidade`, `estado`) VALUES (NULL, '$usuario', '$password', '$email', '$nome', '$sobrenome', '$rg', '$cpf', NULL, '$rua', '$numero', '$cep', '$bairro', '$cidade', '$estado')");
 	
 ?>
