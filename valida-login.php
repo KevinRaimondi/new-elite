e32c48c6f9eee09585ec67f815d07ba4150b482d
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-01 21:48:05 -0300

    Testando serviço de login

---
+++ valida-login.php
@@ -0,0 +1,15 @@
+<?php 
+	require ("conexao.php");
+	$usuario = $_POST['inputUsuario'];
+	$senha = $_POST['inputPassword'];
+	$query = mysqli_query($conn,"SELECT * FROM usuario WHERE login = '$usuario' AND senha = '$senha'");
+	$row = mysqli_num_rows($query);
+	if ($row > 0){
+		session_start();
+		$_SESSION['usuario'] = $_POST['inputUsuario'];
+		$_SESSION['senha'] = $_POST['inputPassword'];
+		header('Location: /planos');
+	}else{
+		header('Location: /login');
+	}
+?>
