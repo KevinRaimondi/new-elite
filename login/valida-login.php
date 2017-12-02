044f3d797fb5a4295beb7903110367b8feeb1af2
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-01 23:54:10 -0300

    testando login

--- login/valida-login.php
+++ login/valida-login.php
@@ -2,11 +2,11 @@
 	require ("/conexao.php");
 	$usuario = $_POST['inputUsuario'];
 	$senha = $_POST['inputPassword'];
-	$query = mysqli_query($conn,"SELECT * FROM usuario WHERE login = '$usuario' AND senha = '$senha'");
+	$query = mysqli_query($conn,"SELECT login, senha  FROM usuario WHERE login = '$usuario' AND senha = '$senha'");
 	$row = mysqli_num_rows($query);
 	if ($row > 0){
 		session_start();
-		$_SESSION['usuario'] = $_POST['inputUsuario'];
+		$_SESSION['login'] = $_POST['inputUsuario'];
 		$_SESSION['senha'] = $_POST['inputPassword'];
 		header('Location: /planos');
 	}else{
