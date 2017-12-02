d0cd3459c83b056faa88c533a403f11bc073598d
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-02 20:55:22 -0300

    teste

--- em_construcao/index.php
+++ em_construcao/index.php
@@ -22,7 +22,7 @@
           </li>
 		  <?php 
 		  
-		  	require ("conexao.php");
+		  	require ("../conexao.php");
 			session_start();
 		  
 		  	if(!isset($_SESSION["login"]) || !isset($_SESSION["senha"])){
