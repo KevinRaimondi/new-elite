45ef71e3b1b75e073df8c8967f560497c1c8e270
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-02 00:57:06 -0300

    correção

---
+++ cad_usuario.php
@@ -0,0 +1,34 @@
+﻿<?php 
+
+	require("conexao.php");
+
+	$email 				= $_POST["email"];
+	$usuario				= $_POST["usuario"];
+	$password			= $_POST["password"];
+	$passwordConfirm		= $_POST["passwordConfirm"];
+	$nome 				= $_POST["nome"];
+	$sobrenome			= $_POST["sobrenome"];
+	$rg					= $_POST["rg"];
+	$cpf					= $_POST["cpf"];
+	
+	$nascimento  		= $_POST["nascimento"];
+	
+	$rua  				= $_POST["rua"];
+	$numero  			= $_POST["numero"];
+	$cep  				= $_POST["cep"];
+	$bairro  			= $_POST["bairro"];
+	$cidade  			= $_POST["cidade"];
+	$estado  			= $_POST["estado"];
+	
+		
+	if(password != passwordConfirm){
+		header('Location: /cadastro/index.php?msg=1"');
+	}
+	
+	if(estado == "select"){
+		header('Location: /cadastro/index.php?msg=2"');
+	}
+			
+	$query = mysqli_query ($conn,"INSERT INTO `usuario` (`id`, `login`, `senha`, `email`, `nome`, `sobrenome`, `rg`, `cpf`, `nascimento`, `rua`, `numero`, `cep`, `bairro`, `cidade`, `estado`) VALUES (NULL, '$usuario', '$password', '$email', '$nome', '$sobrenome', '$rg', '$cpf', NULL, '$rua', '$numero', '$cep', '$bairro', '$cidade', '$estado')");
+	
+?>
