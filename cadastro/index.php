6bcb02de14b836e9aa444c3e92879a822eab4530
Kevin Raimondi <kevin_raimondi@outlook.com>
2017-12-03 02:10:59 -0300

    teste

--- cadastro/index.php
+++ cadastro/index.php
@@ -97,16 +97,6 @@
 				</div>
 			<?php
 			break;
-			case 6:
-			?>
-				<div class="message">
-					<div class="alert alert-danger">
-						<a href="/cadastro" class="close" data-dismiss="alert">&times</a>
-						Favor preencher o campo Nascimento
-					</div>
-				</div>
-			<?php
-			break;
 		}
 	}
 		
